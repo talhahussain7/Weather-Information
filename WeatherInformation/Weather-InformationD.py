e6743dd98c1cb4c9b4255e3b3ebf792afce46d5f
Talha Hussain <talhahussainb@gmail.com>
2018-11-22 00:57:28 +0100

    This program searches for the weather information based on the city input it gets.
    Added a python project.
    By Talha Hussain

---
+++ WeatherInformation/Weather-InformationD.py
@@ -0,0 +1,53 @@
+import requests
+from time import sleep
+
+ABSOLUTE_ZERO = 273
+Choice = "Y"
+
+api_Address = "http://api.openweathermap.org/data/2.5/weather?q="
+api_ID = "&APPID=f9b5b60caa372024f1e419aa12803552"
+
+while Choice != "N":
+    try:
+
+
+            city_name = str(input("Enter the name of the city: "))
+            url = api_Address + city_name +  api_ID
+            jason_data = requests.get(url).json()
+            weather_info = jason_data["weather"][0]["main"]
+            humidity =  jason_data["main"]["humidity"]
+            visibility = jason_data["visibility"]
+            wind_speed = jason_data["wind"]["speed"]
+            country_info =jason_data["sys"]["country"]
+            temp_info = jason_data["main"]["temp"]
+            temp_info = round(temp_info)
+
+            sleep(1)
+            print("-------------------------------------------------------------------------")
+            print("Fetching data...")
+            print("-------------------------------------------------------------------------")
+            sleep(1)
+
+
+            print("Country abbreviation: " + country_info)
+            print("The current weather is: " + weather_info)
+            print("The humidity is: " + str(humidity) + " %")
+            print("The visibility is: " + str(visibility) + "m")
+            print("The wind speed is: " + str(wind_speed) + " km/h")
+            print("the temp is: " + str(temp_info - ABSOLUTE_ZERO) + " Degrees")
+
+            print("-------------------------------------------------------------------------")
+            Choice = input("Do you want to search for another city? Yes(Y)|No(N)")
+            Choice = Choice.upper()
+            print("-------------------------------------------------------------------------")
+    except:
+             print("-------------------------------------------------------------------------")
+             print("Either this country doesn´t exist or the program could not find the data.")
+             print("-------------------------------------------------------------------------")
+             Choice = input("Do you want to search for another city? Yes(Y)|No(N)")
+             Choice = Choice.upper()
+             print("-------------------------------------------------------------------------")
+else:
+    print("-------------------------------------------------------------------------")
+    print("I hope you liked the program :) Thanks")
+    print("-------------------------------------------------------------------------")
